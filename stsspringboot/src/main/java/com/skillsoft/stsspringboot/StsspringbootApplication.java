4fde15a59c423b0526210547039d2401366dbb42
anhdotdo <anhdo157hcl@gmail.com>
2023-06-15 15:44:28 +0700

    first commmit

---
+++ stsspringboot/src/main/java/com/skillsoft/stsspringboot/StsspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.skillsoft.stsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsspringbootApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
